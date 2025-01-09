2415f3f04d3878ab09de3ce825b1b383e473aa15
Johannes Skuterud <johann1301s@icloud.com>
2025-01-10 00:04:18 +0100

    wip

--- example/delta/deltaEffects.ts
+++ example/delta/deltaEffects.ts
@@ -2,13 +2,13 @@ import {createEffect} from '../common/state/state'
 
 export const fetchUserEffect = createEffect({
     effectType: 'takeEvery',
-    actionType: 'fetchOrder',
+    actionType: 'removeOrder',
     handler: ({action, actions}) => {
         actions.echo.fetchUser({id: '', nok: true})
         actions.delta.removeOrder()
 
         action.type
 
-        action.payload
+        action
     }
 })
--- src/types/effect.ts
+++ src/types/effect.ts
@@ -8,13 +8,10 @@ export type TEffect<TDomains extends TDomainsBase, T extends keyof MergeUnion<TD
     actionType: T
     effectType: 'takeLeading' | 'takeEvery' | 'takeLatest'
     handler: (ctx: {
-        action: {
-            type: T
-            payload: (T extends keyof MergeUnion<TDomains['domains'][keyof TDomains['domains']]>
+        action: (T extends keyof MergeUnion<TDomains['domains'][keyof TDomains['domains']]>
                 ? (Parameters<MergeUnion<TDomains['domains'][keyof TDomains['domains']]>[T]>[0] extends { payload: any}
-                    ? Parameters<MergeUnion<TDomains['domains'][keyof TDomains['domains']]>[T]>[0]['payload'] : undefined)
-                : unknown)
-        },
+                    ? {type: T, payload: Parameters<MergeUnion<TDomains['domains'][keyof TDomains['domains']]>[T]>[0]['payload']} : { type: T })
+                : { type: T, payload: unknown}),
         actions: {
             [K in keyof TDomains['domains']]: {
                 [P in keyof TDomains['domains'][K]]: Parameters<TDomains['domains'][K][P]>[0] extends { payload: any } ? (payload: Parameters<TDomains['domains'][K][P]>[0]['payload']) => void : () => void 
