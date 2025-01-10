097bc67265f97c9c924e29c0fa00d2747492d9e8
Johannes Skuterud <johann1301s@icloud.com>
2025-01-10 17:24:42 +0100

    One

--- src/types/domainBase.ts
+++ src/types/domainBase.ts
@@ -1,6 +1,5 @@
 import { TReducerCtx } from "./reducerCtx"
 
-
 export type TDomainsBase =  {
     [domain: string]: {
         [action: string]: (ctx: TReducerCtx<any, any>) => void
--- src/types/effectActionType.ts
+++
@@ -1,3 +0,0 @@
-import { TDomainsBase } from "./domainBase";
-
-export type EffectActionType<Domains extends TDomainsBase, Domain extends keyof Domains> = Domains[Domain]
