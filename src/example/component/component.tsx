1872e16aa1956182825d7b4d9317d67c7b8f7fcc
Johannes Skuterud <johann1301s@icloud.com>
2025-01-12 17:28:27 +0100

    One

--- src/example/component/component.tsx
+++ src/example/component/component.tsx
@@ -5,11 +5,17 @@ import { useActions } from "../state/hook"
 export const Component = () => {
     const {actions, data} = useActions((state) => state)
 
-    return (
-        <button onClick={() => actions.delta.deltaActionWithPayload({
+    const onClick = () => {
+        actions.delta.deltaActionWithPayload({
             count: 1
-        })}>
-            <pre>{JSON.stringify(data, null, 4)}</pre>
+        })
+    }
+
+    return (
+        <button onClick={() => onClick()}>
+            <pre>
+                {JSON.stringify(data, null, 4)}
+            </pre>
         </button>
     )
 }
