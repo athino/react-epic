6996c8d9e8ebc562baba13b51afaa7dbdc8c0903
Johannes Skuterud <johann1301s@icloud.com>
2025-01-12 23:46:56 +0100

    One

--- src/epic/lib/createstore/createStore.ts
+++ src/epic/lib/createstore/createStore.ts
@@ -4,28 +4,22 @@ import { TDomainsBase } from "../../types/domainBase"
 export const createStore = (arg: {
     domains: TDomainsBase
 }) => {
-
     const reducer: any = new Proxy(arg.domains, {
         get(domainsTarget, domainsP) {
             if (typeof domainsP !== 'string') return
             const reducer = domainsTarget[domainsP]
             if (!reducer) return
-
             return (state: any = {}, action: any) => {
                 const actionReducer =  reducer[action.type]
                 if (!actionReducer) return state
-
                 const newState = {...state}
-
                 actionReducer({
                     state: newState,
                     payload: action.payload
                 })
-
                 return newState
             }
         }
     })
-
     return configureStore({ reducer })
 }
