4caf4a6b6702df36fa26e8345e4f956962f6750a
Johannes Skuterud <johann1301s@icloud.com>
2025-01-13 15:39:28 +0100

    wip

--- src/epic/lib/lib.ts
+++ src/epic/lib/lib.ts
@@ -1,4 +1,4 @@
-import { createUtilityHook } from "./createhook/createUtilityHook";
+import { createUtilityHook } from "./createutilityhook/createUtilityHook";
 import { createHookActions } from "./createhookactions/createHookActions";
 import { createProvider } from "./createprovider/createProvider";
 import { createReducer } from "./createreducer/createReducer";
