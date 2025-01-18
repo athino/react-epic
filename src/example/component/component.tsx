d7c4be70eeedf34e48761529311c94e4794c8293
Johannes Skuterud <johann1301s@icloud.com>
2025-01-18 22:31:36 +0100

    wip

--- src/example/component/component.tsx
+++ src/example/component/component.tsx
@@ -4,10 +4,15 @@ import { useActions } from "../state/hook"
 export const Component = () => {
     const {actions} = useActions()
 
+    const onChange = (value: string) => {
+        actions.delta.deltaActionWithPayload({ value })
+    }
+
     return (
         <div>
             <input
                 type='text'
+                onChange={({target}) => onChange(target.value)}
                 value={''}/>
         </div>
     )
