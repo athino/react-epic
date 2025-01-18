04f0b7be51a2bfeac9f6edd05696284b10242656
Johannes Skuterud <johann1301s@icloud.com>
2025-01-18 22:04:42 +0100

    wip

--- src/epic/lib/createactions/createActions.tsx
+++ src/epic/lib/createactions/createActions.tsx
@@ -3,19 +3,21 @@ import { TDomainsBase } from "../../types/domainsBaseType"
 export const createActions = <D extends TDomainsBase>(arg: {
     domains: D
 }) => {
-
-    const mfeaf = Object.entries(arg.domains).map(([domain, entry]) => ({
-        // @ts-ignore
-        [domain]: entry.actions
-    }))
-
-    console.log(mfeaf)
+    const actions = {} as {
+        [K in keyof D]: {
+            [P in Parameters<D[K]>[1] as P['type']]: P extends {payload: any} ? (payload: P['payload']) => void : () => void
+        }
+    }
 
     return {
-        actions: { } as {
-            [K in keyof D]: {
-                [P in Parameters<D[K]>[1] as P['type']]: P extends {payload: any} ? (payload: P['payload']) => void : () => void
+        actions: new Proxy(actions, {
+            get(domainTarget, domainP) {
+                return new Proxy(domainTarget, {
+                    get(what, actionP) {
+                        console.log(actionP)
+                    }
+                })
             }
-        }
+        })
     }
 }
--- src/example/component/component.tsx
+++ src/example/component/component.tsx
@@ -4,14 +4,12 @@ import { useActions } from "../state/hook"
 export const Component = () => {
     const {actions} = useActions()
 
-    actions.delta.deltaActionWithPayload({ value: '' })
-    actions.delta.deltaActionWithoutPayload()
-
     return (
         <div>
             <input
                 type='text'
                 value={''}/>
+                {}
         </div>
     )
 }
