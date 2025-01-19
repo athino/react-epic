a03ed486797ff14ea7f1bcdd8405949ce8217d45
Johannes Skuterud <johann1301s@icloud.com>
2025-01-19 13:35:38 +0100

    wip

--- src/epic/lib/createstore/createStore.ts
+++ src/epic/lib/createstore/createStore.ts
@@ -1,12 +1,23 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, Middleware } from "@reduxjs/toolkit"
 
 export const createStore = (arg: {
     reducer: (s: any, a: any) => any
 }) => {
 
+    const middleware: Middleware = (store) => (next) => (action) => {
+        const result = next(action)
+
+        console.log('EFFECT: ', {state: store.getState(), action})
+        
+        return result
+    };
+
     const store = configureStore({
         reducer: arg.reducer,
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([]),
+        middleware: (getDefaultMiddleware) => {
+            return getDefaultMiddleware()
+                .concat([middleware])
+        }
     })
 
     return { store }
